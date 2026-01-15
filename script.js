7844d1540b2404fcef23761d3976656a499406a5
jaydeb-ds <das294761@gmail.com>
2026-01-15 15:36:43 +0530

    full respostive nav

--- script.js
+++ script.js
@@ -4,4 +4,11 @@ let navlist = document.getElementById("right-nav")
 ham.addEventListener('click',()=>{
 	nav.classList.toggle("active-nav")
 	navlist.classList.toggle("active_nav_list")
+	
+	if (nav.classList.contains('active-nav')) {
+		ham.src = "cross.svg"
+	}
+	else{
+		ham.src = "hambarger.svg"
+	}
 })
